f7c5d13e8a5490ef65fc0c5a92004b481a1db245
berger <berger@10.50.169.18>
2016-08-26 09:41:21 +0200

    Added some values to insert.

--- src/main/db/values.sql
+++ src/main/db/values.sql
@@ -7,11 +7,21 @@
  * Author:  berger
  * Created: Aug 26, 2016
  */
-
+CREATE TABLE PERSON
+  (     PERSONID INT PRIMARY KEY, 
+  	FIRSTNAME VARCHAR(30),
+        LASTNAME VARCHAR(30),
+        BIRTHDATE DATE
+  );
 
 Insert into VOICE (VOICEID,NAME) values (1,'Bass');
-Insert into VOICE (VOICEID,NAME) values (3,'Tenor');
-Insert into VOICE (VOICEID,NAME) values (5,'Sopran');
+Insert into VOICE (VOICEID,NAME) values (2,'Tenor');
+Insert into VOICE (VOICEID,NAME) values (3,'Sopran');
 
-Insert into MEMBER (PERSONID,VOICEID,PHONE,EMAIL) values (1,5,);
-Insert into MEMBER ((PERSONID,VOICEID,PHONE,EMAIL) values (3,'Tenor');
+Insert into PERSON (PERSONID,FIRSTNAME,LASTNAME,BIRTHDATE) values (1,'Doe','John',date('1998-03-25'));
+Insert into PERSON (PERSONID,FIRSTNAME,LASTNAME,BIRTHDATE) values (2,'Mustermann','Max',date('1981-01-12'));
+Insert into PERSON (PERSONID,FIRSTNAME,LASTNAME,BIRTHDATE) values (3,'And','Anders',date('1978-12-04'));
+
+Insert into MEMBER (PERSONID,VOICEID,PHONE,EMAIL) values (1,3,'+45-12345678','john@doe.org');
+Insert into MEMBER (PERSONID,VOICEID,PHONE,EMAIL) values (2,2,'+45-87654321','max@m.org');
+Insert into MEMBER (PERSONID,VOICEID,PHONE,EMAIL) values (3,1,'+45-43218765','anders@and.org');
