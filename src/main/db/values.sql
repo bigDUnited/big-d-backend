c5f08a816aff3d676c88175aa6b2b79a707165ac
berger <berger@10.50.169.18>
2016-08-26 09:29:14 +0200

    Added initial DB schema and some values. Not complete and likely incorrect.

---
+++ src/main/db/values.sql
@@ -0,0 +1,17 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  berger
+ * Created: Aug 26, 2016
+ */
+
+
+Insert into VOICE (VOICEID,NAME) values (1,'Bass');
+Insert into VOICE (VOICEID,NAME) values (3,'Tenor');
+Insert into VOICE (VOICEID,NAME) values (5,'Sopran');
+
+Insert into MEMBER (PERSONID,VOICEID,PHONE,EMAIL) values (1,5,);
+Insert into MEMBER ((PERSONID,VOICEID,PHONE,EMAIL) values (3,'Tenor');
