fd00a9b30964c52d85b974afc4b96de514b7fd52
ToPeter <tomascik.peter@gmail.com>
2016-12-07 14:00:44 +0100

    Contract implementation

---
+++ src/main/java/dk/cphbusiness/mysql/JPAEntityClasses/ContractController.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.cphbusiness.mysql.JPAEntityClasses;
+
+//import dtos.JourneysDTO;
+//import dtos.LocationDTO;
+//import dtos.ReservationSummaryDTO;
+//import dtos.RouteDTO;
+
+/**
+ *
+ * @author redrose
+ */
+public class ContractController {
+    
+}
