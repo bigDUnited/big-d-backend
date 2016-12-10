6c4725bd14505dd9e406bf9323e34f401ce4a2ab
ToPeter <tomascik.peter@gmail.com>
2016-12-10 13:35:18 +0100

    ContractController created; interface implemented; Snapshot v 1.0

--- src/main/java/dk/cphbusiness/mysql/JPAEntityClasses/ContractController.java
+++ src/main/java/dk/cphbusiness/mysql/JPAEntityClasses/ContractController.java
@@ -5,15 +5,36 @@
  */
 package dk.cphbusiness.mysql.JPAEntityClasses;
 
-//import dtos.JourneysDTO;
-//import dtos.LocationDTO;
-//import dtos.ReservationSummaryDTO;
-//import dtos.RouteDTO;
+import dtos.JourneysDTO;
+import dtos.LocationDTO;
+import dtos.ReservationSummaryDTO;
+import dtos.RouteDTO;
+import java.util.List;
 
 /**
  *
  * @author redrose
  */
-public class ContractController {
+public class ContractController implements contractinterface.ContractInterface  {
+
+    @Override
+    public List<LocationDTO> getLocations() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<RouteDTO> getRoutes(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public JourneysDTO getJourney(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ReservationSummaryDTO makeReservation(int i, int i1, String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
